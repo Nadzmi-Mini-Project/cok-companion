47648389bb6a1047d0fc2b15333fa02ee57e7d73
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-11-08 22:57:40 +0800

    added character service to player provider

--- lib/app/player/provider/player/player.provider.dart
+++ lib/app/player/provider/player/player.provider.dart
@@ -1,3 +1,4 @@
+import 'package:cokc/app/character/service/character-base.service.dart';
 import 'package:cokc/app/player/model/create-player.model.dart';
 import 'package:cokc/app/player/provider/player/player.state.dart';
 import 'package:cokc/app/player/service/player-base.service.dart';
@@ -6,20 +7,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final playerProvider =
     StateNotifierProvider.autoDispose((ref) => PlayerProvider(
           playerService: ref.read(playerServiceProvider),
+          characterService: ref.read(characterServiceProvider),
         ));
 
 class PlayerProvider extends StateNotifier<PlayerState> {
   PlayerBaseService playerService;
+  CharacterBaseService characterService;
 
   PlayerProvider({
     required this.playerService,
+    required this.characterService,
   }) : super(PlayerInitialState());
 
   Future getPlayerList() async {
     try {
       state = PlayerLoadingState();
       final playerList = await playerService.getPlayerList();
-      state = PlayerLoadedState(playerList: playerList);
+      final characterList = await characterService.getCharacterList();
+      state = PlayerLoadedState(
+        playerList: playerList,
+        characterList: characterList,
+      );
     } catch (e) {
       state = PlayerErrorState(message: e.toString());
     }
@@ -30,7 +38,11 @@ class PlayerProvider extends StateNotifier<PlayerState> {
       state = PlayerLoadingState();
       await playerService.createPlayer(model);
       final playerList = await playerService.getPlayerList();
-      state = PlayerLoadedState(playerList: playerList);
+      final characterList = await characterService.getCharacterList();
+      state = PlayerLoadedState(
+        playerList: playerList,
+        characterList: characterList,
+      );
     } catch (e) {
       state = PlayerErrorState(message: e.toString());
     }
@@ -41,7 +53,11 @@ class PlayerProvider extends StateNotifier<PlayerState> {
       state = PlayerLoadingState();
       await playerService.removeAllPlayer();
       final playerList = await playerService.getPlayerList();
-      state = PlayerLoadedState(playerList: playerList);
+      final characterList = await characterService.getCharacterList();
+      state = PlayerLoadedState(
+        playerList: playerList,
+        characterList: characterList,
+      );
     } catch (e) {
       state = PlayerErrorState(message: e.toString());
     }
