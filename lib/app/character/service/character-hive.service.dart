1d2c6e3bbf56e7e16ce58e4ccfcc61df61a3fb72
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-11-23 00:20:24 +0800

    finished character hive service declaration

--- lib/app/character/service/character-hive.service.dart
+++ lib/app/character/service/character-hive.service.dart
@@ -1,15 +1,17 @@
 import 'package:cokc/app/character/model/character.model.dart';
 import 'package:cokc/app/character/service/character-base.service.dart';
 import 'package:cokc/database/box/character.box.dart';
-import 'package:cokc/database/constant/table-collection.constant.dart';
 import 'package:hive/hive.dart';
 
 class CharacterHiveService extends CharacterBaseService {
+  final Box<Character> characterBox;
+
+  CharacterHiveService({required this.characterBox}) : super();
+
   @override
   Future<CharacterModel> getCharacterById(String characterId) async {
-    final character = Hive.box<Character>(TableCollection.characters)
-        .values
-        .firstWhere((element) => element.id == characterId);
+    final character =
+        characterBox.values.firstWhere((element) => element.id == characterId);
 
     return Future.value(CharacterModel(
       id: character.id,
@@ -20,8 +22,7 @@ class CharacterHiveService extends CharacterBaseService {
 
   @override
   Future<List<CharacterModel>> getCharacterList() {
-    final characterList =
-        Hive.box<Character>(TableCollection.characters).values;
+    final characterList = characterBox.values;
 
     return Future.value(characterList
         .map(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cokc/app/home/view/home.view.dart';
 import 'package:cokc/common/router/app-router.dart';
 import 'package:cokc/database/box/character.box.dart';
+import 'package:cokc/database/box/player.box.dart';
 import 'package:cokc/database/constant/table-collection.constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,24 +11,33 @@ void main() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(CharacterAdapter());
-  await Hive.openBox<Character>(TableCollection.characters);
+  Hive.registerAdapter(PlayerAdapter());
+
+  if (!Hive.isBoxOpen(TableCollection.characters)) {
+    await Hive.openBox<Character>(TableCollection.characters);
+  }
+  if (!Hive.isBoxOpen(TableCollection.players)) {
+    await Hive.openBox<Player>(TableCollection.players);
+  }
 
   // TODO: find a way to seed initial data using hive
-  // await Hive.box<Character>(TableCollection.characters).add(Character(
-  //   id: 'char-1',
-  //   name: 'Character 1',
-  //   imagePath: 'asset/image/character/sample.png',
-  // ));
-  // await Hive.box<Character>(TableCollection.characters).add(Character(
-  //   id: 'char-2',
-  //   name: 'Character 2',
-  //   imagePath: 'asset/image/character/sample.png',
-  // ));
-  // await Hive.box<Character>(TableCollection.characters).add(Character(
-  //   id: 'char-3',
-  //   name: 'Character 3',
-  //   imagePath: 'asset/image/character/sample.png',
-  // ));
+  if (Hive.box<Character>(TableCollection.characters).length <= 0) {
+    await Hive.box<Character>(TableCollection.characters).add(Character(
+      id: '1',
+      name: 'Character 1',
+      imagePath: 'asset/image/character/sample.png',
+    ));
+    await Hive.box<Character>(TableCollection.characters).add(Character(
+      id: '2',
+      name: 'Character 2',
+      imagePath: 'asset/image/character/sample.png',
+    ));
+    await Hive.box<Character>(TableCollection.characters).add(Character(
+      id: '3',
+      name: 'Character 3',
+      imagePath: 'asset/image/character/sample.png',
+    ));
+  }
 
   runApp(const ProviderScope(
     child: MyApp(),
