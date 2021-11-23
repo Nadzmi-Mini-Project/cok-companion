4402ca8d324939576aa7f8e36eccfbb45a864a86
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-11-23 22:20:10 +0800

    refactored CreatePlayerModel

--- lib/app/player/model/create-player.model.dart
+++ lib/app/player/model/create-player.model.dart
@@ -1,17 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 class CreatePlayerModel extends Equatable {
-  final String id;
   final String characterId;
 
   const CreatePlayerModel({
-    required this.id,
     required this.characterId,
   });
 
   @override
   List<Object?> get props => [
-        id,
         characterId,
       ];
 
