db8653f262f96793645fdff5e415dcfd3d30bfac
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-11-28 20:40:54 +0800

    resolve merge conflict

--- lib/database/seed/character.seed.dart
+++ lib/database/seed/character.seed.dart
@@ -9,18 +9,33 @@ class CharacterSeed extends BaseSeed {
     if (Hive.box<Character>(TableCollection.characters).length <= 0) {
       await Hive.box<Character>(TableCollection.characters).add(Character(
         id: '1',
-        name: 'Character 1',
-        imagePath: 'asset/image/character/sample.png',
+        name: 'Brutius The broken',
+        imagePath: 'asset/image/character/brutius-the-broken.png',
       ));
       await Hive.box<Character>(TableCollection.characters).add(Character(
         id: '2',
-        name: 'Character 2',
-        imagePath: 'asset/image/character/sample.png',
+        name: 'Calamis Earthshaker',
+        imagePath: 'asset/image/character/calamis-earthshaker.png',
       ));
       await Hive.box<Character>(TableCollection.characters).add(Character(
         id: '3',
-        name: 'Character 3',
-        imagePath: 'asset/image/character/sample.png',
+        name: 'Sesharra',
+        imagePath: 'asset/image/character/sesharra.png',
+      ));
+      await Hive.box<Character>(TableCollection.characters).add(Character(
+        id: '4',
+        name: 'Thralir Hillfeet',
+        imagePath: 'asset/image/character/thralir-hillfeet.png',
+      ));
+      await Hive.box<Character>(TableCollection.characters).add(Character(
+        id: '5',
+        name: 'T\'lorra The Blessed',
+        imagePath: 'asset/image/character/tlorra-the-blessed.png',
+      ));
+      await Hive.box<Character>(TableCollection.characters).add(Character(
+        id: '6',
+        name: 'Viliria Ellion',
+        imagePath: 'asset/image/character/vyliria-ellion.png',
       ));
     }
   }
