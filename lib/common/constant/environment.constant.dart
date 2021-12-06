ed07fcfe2dbdc9f6cdaa5f2b4717950ece93d423
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-12-06 22:10:23 +0800

    added null check

--- lib/common/constant/environment.constant.dart
+++ lib/common/constant/environment.constant.dart
@@ -1,4 +1,5 @@
 abstract class Environment {
+  static const envFile = 'env.json';
   static const dev = 'dev';
   static const uat = 'uat';
   static const prod = 'prod';
--- lib/common/env/env.dart
+++ lib/common/env/env.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
 
+import 'package:cokc/common/constant/environment.constant.dart';
 import 'package:flutter/services.dart';
 
 abstract class Env {
   static Map<String, dynamic> _env = {};
 
   static Future init() async {
-    final envString = await rootBundle.loadString('env.json');
-    _env = json.decode(envString) as Map<String, dynamic>;
+    final envString = await rootBundle.loadString(Environment.envFile);
+
+    if (envString.isNotEmpty) {
+      _env = json.decode(envString) as Map<String, dynamic>;
+    }
   }
 
   static String getEnvironment() {
