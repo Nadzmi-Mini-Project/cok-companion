42d0bd387f1431e3e43add5e672824be02227cf7
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-12-07 23:39:37 +0800

    added add & remove resource from old barn service function

--- lib/app/old-barn/service/old-barn-base.service.dart
+++ lib/app/old-barn/service/old-barn-base.service.dart
@@ -1,5 +1,6 @@
 import 'package:cokc/app/old-barn/model/old-barn.model.dart';
 import 'package:cokc/app/old-barn/service/old-barn-hive.service.dart';
+import 'package:cokc/app/resource/model/resource.model.dart';
 import 'package:cokc/database/provider/session-box.provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,4 +12,6 @@ final sessionServiceProvider =
 abstract class OldBarnBaseService {
   Future<OldBarnModel> get();
   Future<OldBarnModel> update(OldBarnModel model);
+  Future<OldBarnModel> addResource(ResourceModel model, int value);
+  Future<OldBarnModel> removeResource(ResourceModel model, int value);
 }
--- lib/app/old-barn/service/old-barn-hive.service.dart
+++ lib/app/old-barn/service/old-barn-hive.service.dart
@@ -1,5 +1,7 @@
 import 'package:cokc/app/old-barn/model/old-barn.model.dart';
 import 'package:cokc/app/old-barn/service/old-barn-base.service.dart';
+import 'package:cokc/app/resource/enum/resource-code.enum.dart';
+import 'package:cokc/app/resource/model/resource.model.dart';
 import 'package:cokc/database/box/old-barn.box.dart';
 import 'package:cokc/database/box/session.box.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -17,11 +19,76 @@ class OldBarnHiveService extends OldBarnBaseService {
   }
 
   @override
-  Future<OldBarnModel> update(OldBarnModel model) {
+  Future<OldBarnModel> update(OldBarnModel model) async {
     final curSession = sessionBox.get(0);
 
     curSession!.oldBarn = OldBarn.fromModel(model);
-    curSession.save();
+    await curSession.save();
+
+    return Future.value(OldBarn.toModel(curSession.oldBarn));
+  }
+
+  @override
+  Future<OldBarnModel> addResource(ResourceModel model, int value) async {
+    final curSession = sessionBox.get(0);
+
+    switch (model.code) {
+      case ResourceCode.wood:
+        curSession!.oldBarn.wood += value;
+        break;
+      case ResourceCode.ore:
+        curSession!.oldBarn.ore += value;
+        break;
+      case ResourceCode.fish:
+        curSession!.oldBarn.fish += value;
+        break;
+      case ResourceCode.linen:
+        curSession!.oldBarn.linen += value;
+        break;
+      case ResourceCode.part:
+        curSession!.oldBarn.itemPart += value;
+        break;
+    }
+
+    await curSession.save();
+
+    return Future.value(OldBarn.toModel(curSession.oldBarn));
+  }
+
+  @override
+  Future<OldBarnModel> removeResource(ResourceModel model, int value) async {
+    final curSession = sessionBox.get(0);
+
+    switch (model.code) {
+      case ResourceCode.wood:
+        curSession!.oldBarn.wood = ((curSession.oldBarn.wood - value) <= 0)
+            ? 0
+            : curSession.oldBarn.wood - value;
+        break;
+      case ResourceCode.ore:
+        curSession!.oldBarn.ore = ((curSession.oldBarn.ore - value) <= 0)
+            ? 0
+            : curSession.oldBarn.ore - value;
+        break;
+      case ResourceCode.fish:
+        curSession!.oldBarn.fish = ((curSession.oldBarn.fish - value) <= 0)
+            ? 0
+            : curSession.oldBarn.fish - value;
+        break;
+      case ResourceCode.linen:
+        curSession!.oldBarn.linen = ((curSession.oldBarn.linen - value) <= 0)
+            ? 0
+            : curSession.oldBarn.linen - value;
+        break;
+      case ResourceCode.part:
+        curSession!.oldBarn.itemPart =
+            ((curSession.oldBarn.itemPart - value) <= 0)
+                ? 0
+                : curSession.oldBarn.itemPart - value;
+        break;
+    }
+
+    await curSession.save();
 
     return Future.value(OldBarn.toModel(curSession.oldBarn));
   }
